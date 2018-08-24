90dd9c6e4742fd15c67f373b0beaf6b1ab87252b
hbhangale3 <hbhangale3@gmail.com>
2018-08-24 23:00:05 +0530

    Add files via upload

---
+++ bluetooth_tx.ino
@@ -0,0 +1,32 @@
+
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(0,1); // RX, TX
+char c;
+
+void setup() {
+
+Serial.begin(9600);
+
+Serial.println("Enter AT commands:");
+
+mySerial.begin(38400);
+
+}
+
+void loop()
+
+{
+
+if (mySerial.available()>0)
+{
+  c= mySerial.read();
+Serial.write(c);
+
+}
+if (Serial.available()>0)
+c=Serial.read();
+mySerial.write(c);
+
+}
