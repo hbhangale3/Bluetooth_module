c9642fb9f4e8172fa8c86da5cc648129eedf6c94
Harshwardhan Bhangale <hbhangale3@gmail.com>
2018-09-02 01:32:38 +0530

    Add files via upload

--- BT_with_DS/BT_with_DS.ino
+++ BT_with_DS/BT_with_DS.ino
@@ -1,56 +1,53 @@
-#include <SoftEasyTransfer.h>
-#include <SoftwareSerial.h>
-SoftwareSerial mySerial(10,11);
+//#include <SoftwareSerial.h>
+// the following code works for two way communication along with the bluetooth_tx code present in the same repository.
+//SoftwareSerial mySerial(10, 11); // RX, TX
+#include <EasyTransfer.h>
+char c;
+EasyTransfer ETpid, ETrpm;
 
+struct RPM_data{
+  int16_t rpm[4];
+  }rpm_data;
 
-struct PWM_data {
-    float pwm[4];
-  };
-
-  //char pwm_data_sent[4];
-  //char pwm_data_received[4];
-  int i,count=0;  
-  SoftEasyTransfer ETin, ETout;
-  PWM_data data1;
-  PWM_data data2;
-  char pwm_data_received[4];
-
+struct PID_data{
+  float Kp[4];
+  float Ki[4];
+  float Kd[4];
+  }pid_data;
+ 
 void setup() {
-  // put your setup code here, to run once:
-Serial.begin(9600);
-mySerial.begin(38400);
-ETout.begin(details(data1), &mySerial);
-ETin.begin(details(data2), &mySerial);
+Serial.begin(38400);
+Serial2.begin(38400);
+Serial.println("Enter pid values");
+ETrpm.begin(details(rpm_data), &Serial2);
+ ETpid.begin(details(pid_data), &Serial2);
+   for(int i=0;i<4;i++){
+    pid_data.Kp[i]=1;
+    pid_data.Ki[i]=3;
+    pid_data.Kd[i]=7;
+    
+    }
 
 }
 
-void loop() {
-    //for(i = 0; i<=3; i++){
-//      while(!(Serial.available()));
-//      if(Serial.available()>0)
-//      data.pwm[i]= Serial.read();
-//      }
-//      for(int i=0;i<=3;i++){
-      //Serial.println(data1.pwm[i]);  
-      //}
-      ETout.sendData();
-      i=0;
-     
-     
-     
-      if(ETin.receiveData())
-      {  
-        for(i=0;i<=3;i++){
-          pwm_data_received[i]=data2.pwm[i];
-          Serial.println(pwm_data_received[i]);
-        }
+void loop(){
+
+  ETpid.sendData();
+  if(ETrpm.receiveData()>0){
+    for(int i=0;i<4;i++){
+      Serial.println(rpm_data.rpm[i]);
       }
-  delay(100);
-}
+    }
+//
+//if (Serial2.available()>0){
+//
+//c= Serial2.read();
+//Serial.println(c);
+//}
+//delay(100);
+//if (Serial.available()>0){
+//c=Serial.read();
+//Serial2.write(c);
+//}
 
-void serialEvent(){
-  while(Serial.available()){
-    data1.pwm[count%4]=Serial.read();
-  }  
-  count++;
 }
